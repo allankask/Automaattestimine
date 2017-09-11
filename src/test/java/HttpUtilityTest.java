4ccd4ead0fcfe48872923f9a6c658a8eb0473e93
Allan Kask <allan.kask96@gmail.com>
2017-09-11 20:41:33 +0300

    WeatherApp projekt

---
+++ src/test/java/HttpUtilityTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class HttpUtilityTest {
+    @Test
+    public void test() { fail(); }
+
+    /*@Test
+    public void makeSureThatGoogleIsUp() {
+        given().when().get("http://www.google.com").then().statusCode(200);
+    }*/
+
+    @Test
+    public void testURL() throws Exception {
+        String strUrl = "http://stackoverflow.com/about";
+
+        try {
+            URL url = new URL(strUrl);
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            urlConn.connect();
+
+            assertEquals(HttpURLConnection.HTTP_OK, urlConn.getResponseCode());
+        } catch (IOException e) {
+            System.err.println("Error creating HTTP connection");
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @Test
+    public void testAPIKey() { fail(); }
+
+    @Test
+    public void testHttpConnection() { fail(); }
+}
